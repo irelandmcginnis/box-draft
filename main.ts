8051834c57fd0a97592c56c51032bd63558a42fc
irelandmcginnis <111801748+irelandmcginnis@users.noreply.github.com>
2022-12-16 22:03:56 -0700

    finuished

--- main.ts
+++ main.ts
@@ -3,8 +3,18 @@ input.onButtonPressed(Button.A, function () {
     range1.showColor(neopixel.colors(NeoPixelColors.Orange))
 })
 input.onPinPressed(TouchPin.P2, function () {
-    servos.P0.run(50)
-    basic.pause(200)
+    servos.P0.setAngle(180)
+    basic.pause(2000)
+    servos.P0.setAngle(0)
+    basic.pause(2000)
+    servos.P0.setAngle(180)
+    basic.pause(2000)
+    servos.P0.setAngle(0)
+    basic.pause(2000)
+    servos.P0.setAngle(180)
+    basic.pause(2000)
+    servos.P0.setAngle(0)
+    servos.P0.setAngle(180)
     servos.P0.stop()
 })
 input.onButtonPressed(Button.AB, function () {
@@ -22,14 +32,11 @@ let range2: neopixel.Strip = null
 let range1: neopixel.Strip = null
 let strip: neopixel.Strip = null
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
-strip = neopixel.create(DigitalPin.P1, 24, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P1, 36, NeoPixelMode.RGB)
 range1 = strip.range(0, 8)
 range2 = strip.range(8, 15)
 range2.showColor(neopixel.colors(NeoPixelColors.Red))
 strip.show()
-basic.forever(function () {
-	
-})
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P2) == 0) {
         basic.showLeds(`
